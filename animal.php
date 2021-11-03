c302220314a8705b6664f3c2826eefe4071d2dd2
ElJuliangas <91574098+ReyCamaron@users.noreply.github.com>
2021-11-03 17:35:37 -0600

    Add files via upload

---
+++ animal.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <title>03 Julian Bautista</title>
+    
+	<link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+	<header>
+		<div class="wrapp">
+			<div class="logo">
+				<a href="#"><img src="img/logo4.png" alt="FalconMasters"></a>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="index.html">Inicio</a></li>
+					<li><a href="animal.html">Paciente</a></li>
+					<li><a href="medicamento.html">Medicamento</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+	<section class="main">
+		<div class="wrapp">
+			<div class="mensaje">
+				<h1>Bienvenidos al registro de paciente</h1>
+			</div>
+
+			<div class="articulo">
+				<article>
+					<p>Nombre de paciente</p>
+					<input type="text">
+					<br/>
+					<p> Edad del paciente  </p>
+					<input type="text">
+					<br/>
+					<p>¿Cuál es su especie</Q></p>
+					<input type="text">
+					<br/>
+					<p>¿Qué raza es?</p>
+					<input type="text">
+					<br>
+					<br>
+					<br>
+					<br>
+					<br>
+				</article>
+			</div>
+
+			<aside>
+				<div class="widget">
+					<h3>Servicios</h3>
+					<ul>
+						<p>Contamos con:</p>
+						<p>Peluqueria</p>
+						<p>Servicio de veterinaria</p>
+						<P>Guarderia</P>
+						<p>¡Y mucho más!</p>
+					</ul>
+				</div>
+			</aside>
+		</div>
+	</section>
+	<footer>
+		<div class="wrapp">
+            <p> No lista 01 </p>
+            <p> // SUBMÓDULO 2
+                Desarrolla aplicaciones Web con conexión a bases de datos</p>
+            <p>https://github.com/elinava-web </p>
+		</div>
+	</footer>
+</body>
+</html>
---
+++ medicamento.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <title>03 Julian Bautista</title>
+    
+	<link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+	<header>
+		<div class="wrapp">
+			<div class="logo">
+				<a href="#"><img src="img/logo4.png" alt="FalconMasters"></a>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="index.html">Inicio</a></li>
+					<li><a href="animal.html">Paciente</a></li>
+					<li><a href="medicamento.html">Medicamento</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+	<section class="main">
+		<div class="wrapp">
+			<div class="mensaje">
+				<h1>Bienvenidos al área de recetas</h1>
+			</div>
+
+			<div class="articulo">
+				<article>
+					<p> ¿Qué especie es? </p>
+					<input type="text">
+					<br/>
+					<p> ¿Qué raza es?</p>
+					<input type="text">
+					<br/>
+					<p> ¿Está vacunado?</p>
+					<input type="text">
+					<br/>
+					<p> ¿Tiene medicamento preescrito?</p>
+					<input type="text">
+					<br>
+					<br>
+					<br>
+					<br>
+					<br>
+				</article>
+			</div>
+
+			<aside>
+				<div class="widget">
+					<h3>Nos enfocamos mayormente en:</h3>
+					<ul>
+						<p>Perros</p>
+						<p>Gatos</p>
+						<p>Animales pequeñitos</p>
+						<p>Y mucho más</p>
+					</ul>
+				</div>
+			</aside>
+		</div>
+	</section>
+	<footer>
+		<div class="wrapp">
+            <p> No lista 01 </p>
+            <p> // SUBMÓDULO 2
+                Desarrolla aplicaciones Web con conexión a bases de datos</p>
+            <p>https://github.com/elinava-web </p>
+		</div>
+	</footer>
+</body>
+</html>
